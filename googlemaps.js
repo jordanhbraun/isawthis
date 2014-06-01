9b85b993e3ed8b91b5d6b9309fbb26ca5d77f62d
jordanhbraun <jordan@greatjob.is>
2014-05-31 21:44:53 -0500

    Added geoRSS to googlemaps.js

--- googlemaps.js
+++ googlemaps.js
@@ -8,7 +8,7 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map-container'), mapOptions);
 
   var georssLayer = new google.maps.KmlLayer({
-    url: 'http://api.flickr.com/services/feeds/geo/?g=322338@N20&lang=en-us&format=feed-georss'
+    url: 'http://isawthis.twilio.baugues.com/twitter.rss?q=isawthis'
   });
   georssLayer.setMap(map);
 }
